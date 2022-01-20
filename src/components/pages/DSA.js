e65a4462632c7ce4a9ca8d81537fac4bfd2dacdc
Vinit Joshi <vinitjoshi.2345@gmail.com>
2022-01-20 19:13:25 +0530

    add a navbar page

---
+++ src/components/pages/DSA.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../home/Navbar";
+
+function DSA() {
+  return (
+    <div>
+      <Navbar />
+      <h2> Data Structure Page</h2>
+    </div>
+  );
+}
+
+export default DSA;
