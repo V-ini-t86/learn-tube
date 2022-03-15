eca6a9b5dd710ae34b46d5769888964ee4d7cd10
Vinit Joshi <49612855+V-ini-t86@users.noreply.github.com>
2022-03-15 18:54:29 +0530

    Revert "added all apis"

--- src/Redux/reducers/index.js
+++ src/Redux/reducers/index.js
@@ -1,14 +1,10 @@
 import { combineReducers } from "redux";
 import authReducers from "./authReducers";
 import errorReducers from "./errorReducers";
-import questions from "../question/questionReducers";
-import userQuestionReducers from "./userQuestionReducers";
-import getQueReducer from "./getQueReducer";
+import questions from "../question/questionReducers"
 
 export default combineReducers({
   auth: authReducers,
   errors: errorReducers,
-  dsa: questions,
-  userQuestions: userQuestionReducers,
-  quest: getQueReducer,
+  dsa:questions,
 });
