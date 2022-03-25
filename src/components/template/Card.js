f85e2a0885a1585f48d469394fda2d7274e8fea4
Vinit Joshi <vinitjoshi.2345@gmail.com>
2022-03-25 14:08:02 +0530

    Card component is updated

--- src/components/template/Card.js
+++ src/components/template/Card.js
@@ -1,44 +1,45 @@
 import React from "react";
 import _ from "lodash";
-import { Card, CardContent, Typography, Box } from "@mui/material";
-import Chip from "@mui/material/Chip";
+import { Box } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import {
+  Card,
+  CardFooter,
+  CardTitle as Title,
+  LearnBtn as Button,
+  Level,
+  Dot,
+  FooterTitle,
+} from "./Components";
 
 function QCard({ que }) {
   const navigate = useNavigate();
-  const card = (
-    <>
-      <CardContent>
-        <Typography
-          sx={{ color: "white", margin: "4px 0px" }}
-          variant="p"
-          component="p"
-          fontSize={18}
-        >
-          {que.name}
-        </Typography>
-        <Chip color="info" label={que.difficulty} />
-      </CardContent>
-    </>
-  );
-
   return (
-    <Box width="auto" onClick={() => navigate(`/dsa/${_.kebabCase(que.id)}`)}>
-      <Card
-        sx={{
-          display: "flex",
-          justifyContent: "left",
-          background: "#74cfa5de",
-          ":hover": {
-            background: "green",
-            cursor: "pointer",
-          },
-        }}
-        variant="outlined"
-      >
-        {card}
+    <>
+      <Card>
+        <Box
+          display="flex"
+          alignItems="center"
+          flexBasis="auto"
+          justifyContent="space-between"
+        >
+          <Box display="flex" alignItems="center">
+            <Title>{que.name}</Title>
+            <span style={{ display: "flex", gap: "10px", marginLeft: "1rem" }}>
+              <Dot />
+              <Level>{que.difficulty}</Level>
+            </span>
+          </Box>
+          <Button onClick={() => navigate(`/dsa/${_.kebabCase(que.id)}`)}>
+            Learn
+          </Button>
+        </Box>
       </Card>
-    </Box>
+      <CardFooter>
+        <FooterTitle>{que.pattern[0]}</FooterTitle>
+        <FooterTitle>{que.companies}</FooterTitle>
+      </CardFooter>
+    </>
   );
 }
 
--- src/components/template/Components.js
+++ src/components/template/Components.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Typography, Box } from "@mui/material";
+import { styled } from "@mui/system";
 import tw from "twin.macro";
 
 export const HeadSubTitle = tw.h4`font-sans font-medium text-2xl text-[#7D3131]`;
@@ -21,3 +22,72 @@ export function Title({ type, underline, variant }) {
     </Typography>
   );
 }
+
+const Card = styled("div")(({ theme }) => ({
+  margin: "1rem 0rem 0rem 0rem",
+  padding: "3rem",
+  width: "auto",
+  background: "rgb(27, 23, 23,0.5)",
+  border: "5.26316px solid #A8A3A3",
+  borderRadius: "35.0877px 0px",
+}));
+
+const CardTitle = styled("h4")(({ theme }) => ({
+  color: "#FFFFFF",
+  fontFamily: "Raleway, sans-serif",
+  fontWeight: 300,
+  lineHeight: "33px",
+  fontSize: "28px",
+}));
+
+const Dot = styled("div")(({ theme }) => ({
+  width: "16px",
+  height: "15px",
+  borderRadius: "50%",
+  background: "#16896D",
+}));
+
+const Level = styled("h4")(({ theme }) => ({
+  color: "white",
+  fontFamily: "Raleway, sans-serif",
+  fontWeight: 300,
+  width: "16px",
+  height: "15px",
+}));
+
+const LearnBtn = styled("button")(({ theme }) => ({
+  fontFamily: "Raleway, sans-serif",
+  color: "black",
+  fontWeight: 600,
+  lineHeight: "25px",
+  fontSize: "21px",
+  borderRadius: "8px",
+  borderColor: "black",
+  background: "#9BA5BF",
+  padding: "0px 15px",
+  transition: "0.4s",
+  cursor: "pointer",
+  "&:hover": {
+    background: "white",
+  },
+}));
+
+const CardFooter = styled(Box)(({ theme }) => ({
+  width: "80%",
+  color: "white",
+  background: "rgb(27, 23, 23,0.5)",
+  border: "5.26316px solid #A8A3A3",
+  borderRadius: "0px 0px 35.0877px 0px",
+  display: "flex",
+  justifyContent: "space-between",
+  padding: "1rem",
+}));
+const FooterTitle = styled("h5")(({ theme }) => ({
+  fontFamily: "Raleway, sans-serif",
+  fontStyle: "normal",
+  fontWeight: 300,
+  fontSize: "17.5439px",
+  lineHeight: "21px",
+}));
+
+export { Card, LearnBtn, Level, Dot, CardTitle, CardFooter, FooterTitle };
