a0e35c627f09828f02db1c2bb183162edf2c9a86
Vinit Joshi <vinitjoshi.2345@gmail.com>
2022-03-28 15:43:09 +0530

    added filter and companies

--- src/components/DSA/companies.js
+++ src/components/DSA/companies.js
@@ -1,30 +1,75 @@
-const company = [
-  { id: 1, title: "Google" },
-  { id: 2, title: "Intuit" },
-  { id: 3, title: "Amazon" },
-  { id: 4, title: "Apple" },
-  { id: 5, title: "Uber" },
-  { id: 6, title: "Bloomberg" },
-  { id: 7, title: "Adobe" },
-  { id: 8, title: "GoldmanSachs" },
-  { id: 9, title: "Airbnb" },
-  { id: 10, title: "Facebook" },
-  { id: 11, title: "Snapchat" },
-  { id: 12, title: "Microsoft" },
-  { id: 13, title: "CapitalOne" },
-  { id: 14, title: "Oracle" },
-  { id: 15, title: "Qualtrics" },
-  { id: 16, title: "Lyft" },
-  { id: 17, title: "BlackRock" },
-  { id: 18, title: "Citadel" },
-  { id: 19, title: "JPMorgan" },
-  { id: 20, title: "Salesforce" },
-  { id: 21, title: "Twitter" },
-  { id: 22, title: "Quora" },
-  { id: 23, title: "Atlassian" },
-  { id: 24, title: "LinkedIn" },
-  { id: 25, title: "MorganStanley" },
-  { id: 26, title: "Pinterest" },
-  { id: 27, title: "Tesla" },
-  { id: 28, title: "Asana" },
+import GoogleIcon from "../../images/company/google.png";
+// import IntuitIcon from "../../images/company/intuit.svg";
+import AmazonIcon from "../../images/company/amazon-pay.png";
+import AppleIcon from "../../images/company/apple.png";
+import UberIcon from "../../images/company/uber.png";
+import AdobeIcon from "../../images/company/adobe.png";
+import GoldmanIcon from "../../images/company/goldman.svg";
+import AirbnbIcon from "../../images/company/airbnb.png";
+import FacebookIcon from "../../images/company/facebook.png";
+import SnapchatIcon from "../../images/company/snapchat.png";
+import MicrosoftIcon from "../../images/company/microsoft.png";
+import CapitalIcon from "../../images/company/capital-one.png";
+import OracleIcon from "../../images/company/oracle.png";
+import LyftIcon from "../../images/company/lyft-icon.png";
+import BlackRockIcon from "../../images/company/black-rock.svg";
+import SalesIcon from "../../images/company/salesforce.png";
+import TwitterIcon from "../../images/company/twitter.png";
+import QuoraIcon from "../../images/company/quora.png";
+import AtlassianIcon from "../../images/company/atlassian.png";
+import LinkedinIcon from "../../images/company/linkedin.png";
+import PinterestIcon from "../../images/company/pinterest.png";
+import TeslaIcon from "../../images/company/tesla.png";
+import AsanaIcon from "../../images/company/asana-circle.png";
+
+export const companyIcons = [
+  { id: 1, title: "Google", icon: GoogleIcon },
+  { id: 3, title: "Amazon", icon: AmazonIcon },
+  { id: 4, title: "Apple", icon: AppleIcon },
+  { id: 5, title: "Uber", icon: UberIcon },
+  // { id: 6, title: "Bloomberg" },
+  { id: 7, title: "Adobe", icon: AdobeIcon },
+  { id: 8, title: "GoldmanSachs", icon: GoldmanIcon },
+  { id: 9, title: "Airbnb", icon: AirbnbIcon },
+  { id: 10, title: "Facebook", icon: FacebookIcon },
+  { id: 11, title: "Snapchat", icon: SnapchatIcon },
+  { id: 12, title: "Microsoft", icon: MicrosoftIcon },
+  { id: 13, title: "CapitalOne", icon: CapitalIcon },
+  { id: 14, title: "Oracle", icon: OracleIcon },
+  // { id: 15, title: "Qualtrics" },
+  { id: 16, title: "Lyft", icon: LyftIcon },
+  { id: 17, title: "BlackRock", icon: BlackRockIcon },
+  // { id: 18, title: "Citadel" },
+  // { id: 19, title: "JPMorgan" },
+  { id: 20, title: "Salesforce", icon: SalesIcon },
+  { id: 21, title: "Twitter", icon: TwitterIcon },
+  { id: 22, title: "Quora", icon: QuoraIcon },
+  { id: 23, title: "Atlassian", icon: AtlassianIcon },
+  { id: 24, title: "LinkedIn", icon: LinkedinIcon },
+  // { id: 25, title: "MorganStanley" },
+  { id: 26, title: "Pinterest", icon: PinterestIcon },
+  { id: 27, title: "Tesla", icon: TeslaIcon },
+  { id: 28, title: "Asana", icon: AsanaIcon },
+];
+export const topic = [
+  { id: 1, title: "Arrays" },
+  { id: 2, title: "BitManipulation" },
+  { id: 3, title: "BFS" },
+  { id: 4, title: "DFS" },
+  { id: 5, title: "UnionFind" },
+  { id: 5, title: "TopologicalSort" },
+  { id: 6, title: "Backtracking" },
+  { id: 7, title: "BinarySearch" },
+  { id: 8, title: "Heap" },
+  { id: 9, title: "BucketSort" },
+  { id: 10, title: "DynamicProgramming" },
+  { id: 11, title: "Trie" },
+  { id: 12, title: "Design" },
+  { id: 13, title: "Greedy" },
+  { id: 14, title: "Fast&SlowPointers" },
+  { id: 15, title: "Intervals" },
+  { id: 16, title: "In-placereversalofalinkedlist" },
+  { id: 17, title: "SlidingWindow" },
+  { id: 18, title: "Sorting" },
+  { id: 19, title: "TwoPointers" },
 ];
